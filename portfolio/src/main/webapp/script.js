1c1ab92ea1324477407eefb2357c4affebbbfadb
Darren Thompson <darren.thompson200@gmail.com>
2020-05-27 17:33:41 -0400

    Add footer

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+$(document).ready(function() {
+
+    let date = new Date();
+    let year = date.getFullYear();
+    $("#copyright").html(`&copy; ${year} Darren Thompson`);
+
+});
