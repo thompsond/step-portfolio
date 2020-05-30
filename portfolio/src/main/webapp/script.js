52bc6e6f85898481f0e847105b76eb9fa9541d60
Darren Thompson <darren.thompson200@gmail.com>
2020-05-29 20:01:08 -0400

    Add comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,24 +16,33 @@
 
 $(document).ready(function() {
 
+    // Create the text for the copyright information
     let date = new Date();
     let year = date.getFullYear();
     $("#copyright").html(`&copy; ${year} Darren Thompson`);
 
+    // Use the height of the header/navigation to create an
+    // offset of the main content
     let $height = $("header").outerHeight(true);
-
     $("#main-container").css("margin-top", `${$height + 30 + "px"}`);
 
+
+    // Calculate the threshold for the scroll dialog
     let threshold = $(document).height() * 0.1;
     let didShowDialog = false;
 
+    // When the user crosses the specified threshold
+    // show a dialog with a link to my personal site. 
     $(window).scroll(function() {
         if($(window).scrollTop() + $(window).height() > $(document).height() - threshold && !didShowDialog) {
             $("#dialog").show();
+            // Additionally the boolean is used to make sure
+            // the dialog is only shown once
             didShowDialog = true;
         }
     });
 
+    // Hide the dialog window when the website link is clicked
     $("#message a").click(function(event) {
         $("#dialog").hide();
     });
